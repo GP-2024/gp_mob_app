9e313bd2b73a9d1cea904b098f25922e86877574
Ahmedhossam <ahmedhossamdev1@gmail.com>
2024-04-30 02:08:30 +0300

    Refactor Separator component styles in Separator.js

--- components/lists/Separator.js
+++ components/lists/Separator.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import colors from '../../config/colors';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import colors from "../../config/colors";
 
 function Separator() {
-	return <View style={styles.separator}></View>;
+    return <View style={styles.separator}></View>;
 }
 
 const styles = StyleSheet.create({
-	separator: {
-		width: '100%',
-		height: 1,
-		backgroundColor: colors.light,
-	},
+    separator: {
+        width: "90%",
+        height: 2,
+        backgroundColor: colors.primary,
+        margin: 30,
+    },
 });
 
 export default Separator;
